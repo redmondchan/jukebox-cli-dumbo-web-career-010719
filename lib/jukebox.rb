ef62ecb63f864b6c71d1cb5bd6dc2bb41b15a6b0
Redmond Chan <redmondchan92@gmail.com>
2019-01-07 04:32:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,7 @@ songs = [
 
 def help
   puts "I accept the following commands:
-  - help : displays this help message
+- help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
 - exit : exits this program"
